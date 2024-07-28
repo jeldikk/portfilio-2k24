825be11c2ed2c479152e427c326ad8c97b82f790
jeldikk <jeldi.kamal2013@gmail.com>
2024-07-28 12:06:29 +0530

    add favicon, brand pic to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import {
   NavbarCollapse,
   NavbarLink,
 } from "flowbite-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
@@ -12,6 +13,13 @@ export default function Header() {
     <header className="sticky top-0">
       <Navbar fluid rounded>
         <NavbarBrand as={Link} href="/">
+          <Image
+            width={30}
+            height={30}
+            src={"/images/cartoon-pic.jpg"}
+            alt="jeldikk brand pic"
+            sizes=""
+          />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Hi, iamkamal
           </span>
