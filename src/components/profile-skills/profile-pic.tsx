1fda2ea65fc757711518e751c88a0362e8c83647
jeldikk <jeldi.kamal2013@gmail.com>
2024-09-10 22:24:51 +0530

    add responsiveness to elements using tailwind classes

--- src/components/profile-skills/profile-pic.tsx
+++ src/components/profile-skills/profile-pic.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function ProfilePic() {
   return (
-    <div className="profile-pic h-[200px] grid place-content-center">
+    <div className="profile-pic h-[200px] grid place-content-center w-full">
       <Image
-        className="rounded-full border-4 border-sky-500 sepia blur-sm"
+        className="rounded-full border-4 border-purple-600"
         alt="profile picture"
         src={"/images/profile-pic.jpg"}
         height={200}
