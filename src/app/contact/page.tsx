19374a49bbc8b566f75bb91ff59dc90df0ab325d
jeldikk <jeldi.kamal2013@gmail.com>
2024-09-10 23:33:48 +0530

    add small screen sizes to contact me page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactMeForm from "@/components/contact/contact-me.form";
 export default function ContactPage() {
   return (
     <div className="contact-page">
-      <div className="container w-1/2 mx-auto">
+      <div className="container p-4 md:w-1/2 mx-auto">
         <h1 className="text-2xl font-bold text-center my-2">Contact Me</h1>
         <ContactMeForm></ContactMeForm>
       </div>
